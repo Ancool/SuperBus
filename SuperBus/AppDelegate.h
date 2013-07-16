4ac03fc6c512e0a6c60c2be413a2eb5555bf3362
Alan <ancool@live.com>
2013-07-16 10:24:02 +0800

    Initial Commit

---
+++ SuperBus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SuperBus
+//
+//  Created by Alan on 13-7-16.
+//  Copyright (c) 2013年 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
